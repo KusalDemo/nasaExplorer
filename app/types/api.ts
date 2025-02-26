e7b3249a908a4f4a0ec6a9c886966e57c6152fac
KusalDemo <kusalgunasekara2002@gmail.com>
2025-02-26 15:04:52 +0530

    Implemented type RoverManifest

--- app/types/api.ts
+++ app/types/api.ts
@@ -40,3 +40,14 @@ export interface EarthImage {
     date: string;
     url: string;
 }
+
+export interface RoverManifest {
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+    max_sol: number;
+    max_date: string;
+    total_photos: number;
+    photos: MarsRoverPhoto[];
+}
